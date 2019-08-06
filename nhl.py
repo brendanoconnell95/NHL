ef319d533eb5bd4762aa74c6f2221e0233535c2a
Brendan O <obren800@vt.edu>
2019-08-06 13:41:40 -0400

    Changed to use datetime date objects in the schedule and starting backtesting schedule

--- nhl.py
+++ nhl.py
@@ -2,30 +2,36 @@
 
 import csv
 import math
+from datetime import datetime
 
 schedule = {}
 team_list = []
 ELO_list = {}
 start_date = '2017-10-04'
 end_date = '2017-11-04'
-k_factor = 30
+k_factor = 10
 
-with open("NHL1819.csv", newline='') as datafile: 
+with open("NHL_scores_1819.csv", newline='') as datafile: 
 	puckreader = csv.DictReader(datafile, fieldnames=['Date','Visitor','VG','Home','HG'])
-	#first pass to create team list and schedule
+	
 	for row in puckreader:
 		if row['Home'] not in team_list: 
 			team_list.append(row['Home'])
 	
-		if row['Date'] not in schedule: 
-			schedule.update({row['Date'] : []})
+		cur_date = datetime.strptime(row['Date'],"%Y-%m-%d").date()
+		if cur_date not in schedule: 
+			schedule.update({cur_date: []})
 			#schedule.sort()
 		
-		if row['Home']+row['HG']+row['Visitor']+row['VG'] not in schedule[row['Date']]:
-			new_list = schedule[row['Date']]
-			new_list.append(row['Home']+ ": " +row['HG']+ ", " +row['Visitor']+ ": " +row['VG'])
-			schedule.update({row['Date'] : new_list})
-		
+		#if match is not in schedule, add it
+		#Match will never be! May be useful to check in future though
+		match = row['Home']+ ": " +row['HG']+ ", " +row['Visitor']+ ": " +row['VG']
+		if match not in schedule[cur_date]:
+			a_list = schedule[cur_date]
+			a_list.append(match)
+			schedule.update({cur_date : a_list})
+		else: 
+			print("skipped match")
 for team in team_list: 
 	ELO_list.update({team: 1500})
 		
@@ -59,12 +65,65 @@ for day in schedule:
 	#need to read the results of the day and update the ELO rankings
 	matches = schedule[day]
 	#print(day, matches)
-	#determine the result of the match
-	
 
 	for match in matches: 
 		#print(match)	
 		updateELO(match)
 
 
-print(sorted(ELO_list.items(), key=lambda x: x[1], reverse=True))
+# for elem in sorted(ELO_list.items(), key=lambda x: x[1], reverse=True): 
+	# print(elem)
+
+#-------------------------BACK TESTING---------------------------------
+with open("NHL_odds_1819.csv", newline="", encoding='utf-8-sig') as oddsfile: 
+	oddsreader = csv.reader(oddsfile)
+	for row in oddsreader:
+		#need to convert dates to create schedule
+
+		if len(row[0]) == 4: 
+			cur_month = row[0][0:2]
+			cur_day = row[0][2:4]
+			cur_year = "2018"
+		else:
+			cur_year = "2019"
+			#need to append a '0' to the month
+			cur_month = "0" + row[0][0]
+			cur_day = row[0][1:3]
+		
+		cur_date = datetime.strptime(cur_month+cur_day+cur_year, "%m%d%Y").date()
+		print(cur_date)
+
+		#opening lines are 5th col, closing are 6th
+		#print(row[4])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
